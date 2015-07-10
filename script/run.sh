0fe2133f144c7bf3f29bce674c1ed02e2e249914
brownman <brownman556@gmail.com>
2015-07-10 16:10:36 +0300

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,8 +1,9 @@
  
  
  #http://www.evanjones.ca/buffer-overflow-101.html
-
- cmd="gcc -ggdb -fno-stack-protector -U_FORTIFY_SOURCE -Wa,--execstack -o SimpleDemo $dir_c/SimpleDemo.c"
+dir_bin=$dir_root/bin
+file=$dir_bin/SimpleDemo
+ cmd="gcc -ggdb -fno-stack-protector -U_FORTIFY_SOURCE -Wa,--execstack -o $file $dir_c/SimpleDemo.c"
  commander "$cmd"
- ls -ls
- ./SimpleDemo
+ ls -l $file
+ bash -c $file
