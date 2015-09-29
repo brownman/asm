bc819ab970bb993d31a00c7802c69941f49ea664
brownman <brownman@linnovate.net>
2015-09-29 16:38:15 +0300

    x

--- script/run.sh
+++ script/run.sh
@@ -4,7 +4,7 @@
 dir_bin=$dir_root/bin
 file=$dir_bin/SimpleDemo
  cmd="gcc -ggdb -fno-stack-protector -U_FORTIFY_SOURCE -Wa,--execstack -o $file $dir_c/SimpleDemo.c"
- commander "$cmd"
+ commander "$cmd 1 3"
  ls -l $file
  bash -c $file
  
